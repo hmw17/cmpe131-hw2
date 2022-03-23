2abe69e6c7702fd2b7870b5515869a23b1a67974
Haomiao Wang <haomiao.wang@sjsu.edu>
2022-03-22 23:03:30 -0700

    1

--- timeit.py
+++ timeit.py
@@ -5,7 +5,7 @@ def calculate_time(func):
         start = time.time()
         func()
         end = time.time()
-        print "Total time is ", end - start
+        print "Total time ", end - start
 
     return cat
 
